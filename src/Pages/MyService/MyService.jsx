d67ccee83bd584c860091593f173977d4ee1fa31
kawser Bappy <bappydu2015@gmail.com>
2025-11-10 17:25:04 +0600

    Add my service to UI

--- src/Pages/MyService/MyService.jsx
+++ src/Pages/MyService/MyService.jsx
@@ -1,7 +1,39 @@
-import React from "react";
-
+import React, { use, useEffect, useState } from "react";
+import { AuthContext } from "../../Context/AuthContex";
+import Loader from "../../Components/Loader";
+import ProductCard from "./../../Components/ProductCard";
+// http://localhost:3000/my-services?email=bappydu2015@gmail.com
+// {
+//         headers: {
+//           authorization: `Bearer ${user.accessToken}`,
+//         },
+//       }
 const MyService = () => {
-  return <div>myservices</div>;
+  const { user } = use(AuthContext);
+  const [services, setServices] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/my-services?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setServices(data);
+        setLoading(false);
+      });
+  }, [user]);
+
+  if (loading) {
+    return <Loader></Loader>;
+  }
+  return (
+    <div className="container mx-auto py-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        {services.map((service) => (
+          <ProductCard key={service._id} service={service}></ProductCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyService;
